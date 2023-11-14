f2954931c07795627fb6be40507ea7772a099f95
walsptr <syawal.als.com@gmail.com>
2023-11-14 13:47:54 +0700

    back

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express()
 
 let expressjs = require('express');
 
-let app = expressjs();  // Compliant
-app.disable("x-powered-by");
+let app1 = expressjs();  // Compliant
+app1.disable("x-powered-by");
 
 let helmet = require("helmet");
-let app = expressjs(); // Compliant
-app.use(helmet.hidePoweredBy());
+let app2 = expressjs(); // Compliant
+app2.use(helmet.hidePoweredBy());
 
 
 const path = require('path')
